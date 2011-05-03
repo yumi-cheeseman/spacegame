c22c3e00c2218684c6133d8b804cef6535f34236
Maverick Motherfucker <MMavipc@hostedyou.com>
2011-05-02 19:06:18 -0700

    Added cpp files to project, and added visual studio stuff to .gitignore

--- main.cpp
+++ main.cpp
@@ -12,7 +12,12 @@ int main(int argc, char** argv)
 			std::cout << "Hello, Linux." << std::endl;
 		#endif
 	#endif
-	
+
+	#ifdef PLATFORM_WINDOWS
+		std::cout << "Press enter to continue" << std::endl;
+		std::cin.ignore(1000, '\n');
+	#endif
+
     return 0;
 }
 
